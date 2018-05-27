5d9801184a1f0b0ddcf5c739d6e226b64fc8d567
HardPlant <abc7988se@naver.com>
2018-05-27 05:37:03 -0700

    updated shell, removed peer docker
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- hackerton-physical-network/1.make_crypto.sh
+++ hackerton-physical-network/1.make_crypto.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ $1 = "generate"]; then
+    rm -rf crypto-config
+    rm -rf channel-artifacts
     cryptogen generate --config=./crypto-config.yml
 else
     cryptogen extend --config=./crypto-config.yml
