beae157ff5b911477f028ddaba4b4295759428ff
HardPlant <abc7988se@naver.com>
2018-05-27 04:26:08 -0700

    updated make_network_card
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- 1.make_network_card.sh
+++ 1.make_network_card.sh
@@ -7,11 +7,11 @@ FABRIC_NETWORK_NAME=fabric-network
 
 PEER_ADMIN=PeerAdmin@fabric-network
 
-composer card create -p connection.json\
--u PeerAdmin\
--c Admin@org1.example.com-cert.pem\
--k 114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk\
--r PeerAdmin -r ChannelAdmin
+cp ./hackerton-physical-network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/* .
+PRIV_KEY=$(ls ./hackerton-physical-network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/)
+cp ./hackerton-physical-network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/* .
+
+composer card create -p connection.json -u PeerAdmin -c Admin@org1.example.com-cert.pem -k $PRIV_KEY -r PeerAdmin -r ChannelAdmin
 
 # 결과물인 카드를 import한다
 composer card import -f $PEER_ADMIN.card
